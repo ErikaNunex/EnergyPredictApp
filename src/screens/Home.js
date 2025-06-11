4193d8fa3a321a87a77137efd6269561f1c22fe6
ErikaNunex <erikanunes0512@gmail.com>
2025-06-11 02:28:21 -0300

    feat: addd logo

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,13 +1,11 @@
-import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity, Image } from "react-native";
 
 export default function Home({ navigation }) {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Energy Predict App ⚡</Text>
-      {/* <Button
-        title="Iniciar simulação."
-        onPress={() => navigation.navigate("Main")}
-      /> */}
+      <Image source={require("../assets/logo.png")} style={styles.logo} />
+      <Text style={styles.title}>Energy Predict</Text>
+
       <View style={styles.buttonRow}>
         <TouchableOpacity
           style={styles.Initbutton}
@@ -24,20 +22,23 @@ const styles = StyleSheet.create({
   container: { flex: 1, alignItems: "center", justifyContent: "center" },
   title: { fontSize: 22, fontWeight: "bold", marginBottom: 20 },
   buttonRow: {
-    flexDirection: "row",
     alignItems: "center",
-    gap: 8,
-    justifyContent: "space-around",
+    marginTop: 10,
   },
   Initbutton: {
     backgroundColor: "#F27405",
+
+    borderRadius: 25,
     paddingVertical: 12,
     paddingHorizontal: 50,
-    borderRadius: 25,
   },
   initButtonText: {
     color: "#fff",
     fontSize: 16,
     fontWeight: "bold",
+  },
+  logo: {
+    width: 150,
+    height: 150,
   },
 });
